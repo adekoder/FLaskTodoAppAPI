fb1b56b88f565b81e2f5cb3296be5e60e0d76c6b
adekoder <adwumiogunbiyi@gmail.com>
2018-01-25 13:44:43 +0100

    create api url structure

--- app/api/views.py
+++ app/api/views.py
@@ -2,7 +2,36 @@ from  flask import jsonify, request, make_response
 from . import api
 
 
-@api.route('/index')
-def index():
-    return 'index'
+@api.route('/login', methods=['POST'])
+def login():
+    pass
+
+@api.route('/signup', methods=['POST'])
+def sign_up():
+    pass 
+
+@api.route('/user', methods=['GET'])
+def users():
+    pass 
+
+@api.route('/user/<string:user_id>', methods=['GET'])
+def get_user(user_id):
+    pass
+
+@api.route('/user/todo', methods=['GET'])
+def get_user_todo():
+    pass
+
+@api.route('/user/todo', methods=['POST'])
+def create_todo():
+    pass
+
+@api.route('/user/todo/todo_id', methods=['PUT'])
+def update_todo_status():
+    pass
+
+@api.route('/user/todo/todo_id', methods=['DELETE'])
+def delete_todo_item():
+    pass
+
 
