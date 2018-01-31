119bef675715a7071232df5e83251e85e079bd94
adekoder <adwumiogunbiyi@gmail.com>
2018-01-31 12:12:00 +0100

    added registrtion function

--- app/api/contrl/registration.py
+++ app/api/contrl/registration.py
@@ -1,18 +1,21 @@
-from api.utilities import generate_random_string
+from app.api.utilities import generate_random_string
 from app.model.user import User
-
+from .authentication import Authentication
 class Registration():
     
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, request):
+        self.__data = request.json
+        self.username = self.__data['username']
+        self.email = self.__data['email']
+        self.password = self.__data['password']
 
 
-    def sigeUp(self):
-        user = User()
-        user.user_id = generate_random_string(10)
-        user.username = self.username
-        user.email = self.email
-        user.password = self.password
-        user.save()
+    def create_user(self):
+        self.user = User()
+        self.user.user_id = generate_random_string(10)
+        self.user.username = self.username
+        self.user.email = self.email
+        self.user.password = self.password
+        self.user.save()
+        return Authentication.auto_login(self.user)
+
--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,16 @@
 from  flask import jsonify, request, make_response
 from . import api
 from .validators import *
-from .resource_schema import validation_error_schema
+from .resource_schema import validation_error_schema, header_error_schema
+from .contrl.registration import Registration
+from .decorators import token_required
+
+
+@api.before_request
+def content_type_json():
+    if not request.headers['content-type'] == 'application/json':
+        return header_error_schema('CONTENT_TYPE_ERROR',
+                'content_type must be set to application/json')
 
 @api.route('/login', methods=['POST'])
 def login():
@@ -12,17 +21,17 @@ def sign_up():
     inputs = RegistrationValidator(request)
     if not inputs.validate():
         return validation_error_schema(inputs.errors)
+    registration = Registration(request)
+    return registration.create_user()
 
-    return jsonify(status=True)
-    
-
-@api.route('/user', methods=['GET'])
+@api.route('/users', methods=['GET'])
 def users():
-    pass 
+    pass
 
 @api.route('/user/<string:user_id>', methods=['GET'])
-def get_user(user_id):
-    pass
+@token_required
+def get_user(current_user, user_id):
+    return jsonify(current_user)
 
 @api.route('/user/todo', methods=['GET'])
 def get_user_todo():
